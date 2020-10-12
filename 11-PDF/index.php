d5fae3482a756673f7b6740aed52c2211d6f3e2d
berlybeer <b.pumacajia.cz@gmail.com>
2020-10-12 18:40:02 -0500

    Demonstrating coordinates and line management

--- 11-PDF/index.php
+++ 11-PDF/index.php
@@ -7,7 +7,8 @@ $pdf = new FPDF();
 $pdf->addPage();
 
 $pdf->setFont("Arial", 'B', 16);
-$pdf->cell(40, 10, "Hello Out There!");
+$pdf->setFillColor(230,230,230);
+$pdf->cell(90, 10, "Hello Out There!", 1, 0, 'C', true);
 
 $pdf->output();
 
