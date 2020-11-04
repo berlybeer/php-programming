67c823a9511a616cdb9606b9b406bd83102ed8c7
berlybeer <b.pumacajia.cz@gmail.com>
2020-11-04 14:00:36 -0500

    Performance tuning

---
+++ 15-App-Techniques/09-profiling.php
@@ -0,0 +1,16 @@
+<?php 
+
+ob_start();
+
+$start = microtime(true);
+
+phpinfo();
+
+$end = microtime(true);
+
+ob_end_clean();
+
+echo "phpinfo() took " . ($end - $start) . " seconds to run.\n";
+
+
+ ?>
